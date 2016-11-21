a0890b199d100e19a09cb7116fa7f367db1f426d
Daniel North <Daniel North>
2016-11-20 20:47:04 -0700

    Add touch capabilities

--- 2D_Procedural_Surivval/Assets/Scripts/Player.cs
+++ 2D_Procedural_Surivval/Assets/Scripts/Player.cs
@@ -21,6 +21,7 @@ public class Player : MovingObject {
 
     private Animator animator;
     private int food;
+    private Vector2 touchOrigin = -Vector2.one; //A point offscreen
 
 	// Use this for initialization
 	protected override void Start ()
@@ -47,11 +48,39 @@ public class Player : MovingObject {
         int horizontal = 0;
         int vertical = 0;
 
+      #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
 
         if (horizontal != 0)
             vertical = 0;
+
+      #else 
+
+        if(Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0]; //Just get the first touch
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    vertical = y > 0 ? 1 : -1;
+            }
+        }
+
+      #endif
 
         if(horizontal != 0 || vertical != 0)
         {
